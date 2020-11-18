5d67625a9b905a69f5af5add897e86dbbbc3c35f
Lan Jin <lan.jin@ucalgary.ca>
2020-11-17 19:33:24 -0700

    Initial Commit

---
+++ FoodSearchApp/FoodSearchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodSearchAppApp.swift
+//  FoodSearchApp
+//
+//  Created by Lan Jin on 2020-11-17.
+//
+
+import SwiftUI
+
+@main
+struct FoodSearchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
